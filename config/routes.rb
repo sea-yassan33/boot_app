cc9319384882548994ed9e833d4671b1cbcb04d1
sea-yassan33 <a.yasuda_tr51@me.com>
2022-12-30 21:09:41 +0900

    登録フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     root to: 'tops#index'
     resources :tops, only: [:index]
-    resources :forms, only: [:index]
+    resources :forms
     resources :a_parts, only: [:index]
   end
 end
